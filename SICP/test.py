35abfb5af9c21ab1bc96ed692056e6394b64c2e3
Albert Chen <albert.chen.dao@gmail.com>
2017-01-04 23:13:03 +0800

    delete old sicp

--- SICP/test.py
+++
@@ -1,9 +0,0 @@
-from __future__ import division
-def sqrt(guess, x):
-    if abs(guess * guess - x) > 0.001:
-        return sqrt((guess + x / guess)/2, x)
-    else:
-        return guess
-
-if __name__ == '__main__':
-    print sqrt(1, 4)
